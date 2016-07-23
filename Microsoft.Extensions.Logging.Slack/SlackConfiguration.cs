0c8ca883e2280933cfeff9d2a2c225ed905686b3
Ugo Lattanzi <imperugo@gmail.com>
2016-07-23 17:54:59 +0000

    removed application name from configuration

--- Microsoft.Extensions.Logging.Slack/SlackConfiguration.cs
+++ Microsoft.Extensions.Logging.Slack/SlackConfiguration.cs
@@ -6,6 +6,5 @@ namespace Microsoft.Extensions.Logging.Slack
 	{
 		public Uri WebhookUrl { get; set; }
 		public LogLevel MinLevel { get; set; }
-		public string ApplicationName { get; set; }
 	}
 }
